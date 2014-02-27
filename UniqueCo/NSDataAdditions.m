902275671cca5bb105297f748ccb3f61a3beda74
Ernesto <ernestfreyre@gmail.com>
2014-02-27 14:28:12 -0500

    Updated

---
+++ UniqueCo/NSDataAdditions.m
@@ -0,0 +1,13 @@
+//
+// Created by Ernesto Freyre on 23/10/13.
+// Copyright (c) 2013 Ernesto Freyre. All rights reserved.
+//
+
+
+#import "NSDataAdditions.h"
+
+
+@implementation NSDataAdditions {
+
+}
+@end
---
+++ UniqueCo/NSStringAdditions.h
@@ -0,0 +1,11 @@
+//
+// Created by Ernesto Freyre on 23/10/13.
+// Copyright (c) 2013 Ernesto Freyre. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSStringAdditions : NSObject
+@end
